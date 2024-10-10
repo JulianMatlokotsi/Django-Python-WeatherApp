fda98e41e376270643619056616f61642e07d990
Julian Matlokotsi <Matlokotsijulian@gmail.com>
2024-10-10 10:49:59 +0200

    Updated project files

--- OpenWeather/SkyCast/SkyCast/settings.py
+++ OpenWeather/SkyCast/SkyCast/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-rbx+r257w@d9h#ar89vst3jkl00vbzcf+d(-%kp=7db4ofp8+w'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
